992839f053902165c848ea482a36e640aa4da3f9
kostytsAleksandr <kostyts.aleksandr@gmail.com>
2021-02-19 19:47:15 +0200

    addProjects

---
+++ Cars.Data/Models/Car.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cars.Data.Models
+{
+    class Car
+    {
+        public int CountWheels { get; set; }
+
+        public string TypeSuspension { get; set; }
+
+        public int WheelDiameter { get; set; }
+    }
+}
